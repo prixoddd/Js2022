8712e0a71141c96450100a7ce399ab0f7a4f91c6
Nikita <nikita.derjugin@gmail.com>
2022-10-15 16:04:24 +0300

    firstlesson

--- Js/script.js
+++ Js/script.js
@@ -1,58 +1,21 @@
-"use sctrict";
+"use strict";
 
-// alert('Hello');
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
 
-// const result = confirm('Are you here?');
-// console.log(result);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genders: [],
+    privat: false
+};
 
-const answer = prompt("Вам есть 18?", "18");
-console.log(typeof(answer));
+const a = prompt("Один из последних фильмов", ""),
+      b = prompt("На сколько вы его оцените?", ""),
+      c = prompt("Один из последних фильмов", ""),
+      d = prompt("На сколько вы его оцените?", "");
 
-// let number = 4.5;
-// const person = "Alex";
-// const bool = true;
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-
-// const obj = {
-//     name: "John", //ключ: "значение"
-//     age: 25,
-//     isMarried: false
-// };
-
-// console.log(obj.name);
-
-// let arr = ['plum.png, orange.jpeg, apple.bmp', [], {}];
-
-// const array = [1, 2, 3]; //массив
-// console.log(array[1]);
-
-// const object = { //объект
-//     'Anna': 500,
-//     'Jane':600
-// };
-
-// // object.b = '123';
-// object['b'] = '123';
-
-// console.log(object.b);
-
-
-
-
-// let number = 5;
-// const leftBorderWidth = 1;
-
-// number = 10;
-// console.log(number);
-
-
-// const obj = {
-//     a: 50
-// };
-
-// obj.a = 10;
-// console.log(obj.a);
-
-// {
-//     let result = 50;
-// }
+console.log(personalMovieDB);
