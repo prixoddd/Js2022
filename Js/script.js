5a574577cdcf8e11c2ef8352207c915a5ebbd02a
Nikita <nikita.derjugin@gmail.com>
2022-10-15 14:17:16 +0300

    first commit

---
+++ Js/script.js
@@ -0,0 +1,58 @@
+"use sctrict";
+
+// alert('Hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+const answer = prompt("Вам есть 18?", "18");
+console.log(typeof(answer));
+
+// let number = 4.5;
+// const person = "Alex";
+// const bool = true;
+
+
+// const obj = {
+//     name: "John", //ключ: "значение"
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj.name);
+
+// let arr = ['plum.png, orange.jpeg, apple.bmp', [], {}];
+
+// const array = [1, 2, 3]; //массив
+// console.log(array[1]);
+
+// const object = { //объект
+//     'Anna': 500,
+//     'Jane':600
+// };
+
+// // object.b = '123';
+// object['b'] = '123';
+
+// console.log(object.b);
+
+
+
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 10;
+// console.log(obj.a);
+
+// {
+//     let result = 50;
+// }
